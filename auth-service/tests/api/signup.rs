82717e16f22b9e671648ea597f9034f621e2c9fc
csaba kiraly <cskiraly27@gmail.com>
2025-07-28 13:24:14 +0200

    [Sprint 2][Task 2] feat: signup route error cases

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,4 @@
-use auth_service::routes::SignupResponse;
+use auth_service::{routes::SignupResponse, ErrorResponse};
 
 use crate::helpers::{get_random_email, TestApp};
 
@@ -64,5 +64,72 @@ async fn should_return_201_if_valid_input() {
             .await
             .expect("Could not deserialize response body to UserBody"),
         expected_response
+    );
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let invalid_users = [
+        serde_json::json!({
+            "email": "",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email.replace("@", ""),
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "pwd1234",
+            "requires2FA": true
+        }),
+    ];
+
+    for invalid_user in invalid_users.iter() {
+        let response = app.post_signup(invalid_user).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            invalid_user
+        );
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to UserBody")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+    let test_user = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+    app.post_signup(&test_user).await;
+    let response = app.post_signup(&test_user).await;
+
+    assert_eq!(response.status().as_u16(), 409);
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned(),
     );
 }
